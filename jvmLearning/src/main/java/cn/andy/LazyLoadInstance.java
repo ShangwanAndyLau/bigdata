50eb0b8455c6bf187e0710e32efac80df43ff98e
刘先彬 <18221192526@163.com>
2018-07-28 11:00:51 +0800

    jvm学习代码提交

---
+++ jvmLearning/src/main/java/cn/andy/LazyLoadInstance.java
@@ -0,0 +1,18 @@
+package cn.andy;
+
+/**
+ * 单例延迟初始化：
+ * 类的加载：当遇到调用静态变量的指令时，初始化该变量指向的类。
+ */
+public class LazyLoadInstance {
+
+    private LazyLoadInstance(){};
+
+    private static  class LazyLoader{
+        static  final LazyLoadInstance INSTANCE = new LazyLoadInstance();
+    }
+
+    public  static  final LazyLoadInstance getInstance(){
+        return LazyLoader.INSTANCE;
+    }
+}
